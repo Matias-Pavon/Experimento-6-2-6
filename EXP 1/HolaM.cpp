8ab906fb85bd3a6ece3f988139ae75bea7dd2814
Matias-Pavon <74216184+Matias-Pavon@users.noreply.github.com>
2020-11-27 17:49:43 -0400

    cambio 2.0

--- EXP 1/HolaM.cpp
+++ EXP 1/HolaM.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 
 int main() {
-	cout << "Hola Mundo";
+	cout << "Hola Mundo"<< endl;
 	cout << "experimento 6-2-6, preueba 1 realizada";
 	cout << "subir una actualizacion de codigo";
+	cout << "detectando 2 errores en es sistema de actualizacion";
 
 	return 0;
 }
